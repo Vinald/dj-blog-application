1df33ec2497654a0bd216377f4685b0cc0da712b
[vinald] <okiror1vinald@gmail.com>
2026-01-18 07:50:58 +0300

    models

--- blog_project/blog/admin.py
+++ blog_project/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "content", "date_posted")
+    search_fields = ("title", "content", "author")
+    list_filter = ("date_posted", "author")
+
+
+admin.site.register(Post, PostAdmin)
--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
+
+urlpatterns = [
+    
+]
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('register/', user_views.register, name='register'),
+    # path('login/', user_views.login_view, name='login'),
+    # path('logout/', user_views.logout_view, name='logout'),
 ]
