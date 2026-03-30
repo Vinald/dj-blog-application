1ae8e5572c948c231306c1209e6f42703d634382
[vinald] <okiror1vinald@gmail.com>
2026-03-30 20:48:39 +0300

    navbar

--- post/urls.py
+++ post/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='post-index'),
+    path('', views.index, name='post-home'),
     path('about/', views.about, name='post-about'),
     path('posts/', views.posts, name='posts'),
     path('posts/<slug:slug>/', views.post_detail, name='post_detail'),
--- post/views.py
+++ post/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from django.template.defaultfilters import title
 
 
 def index(request):
     context = {
         'posts': []  # Empty list for now, populate when models are created
     }
-    return render(request, "post/index.html", context)
+    return render(request, "post/index.html", {"context": context, "title": "Home"})
 
 
 def about(request):
@@ -13,8 +14,8 @@ def about(request):
 
 
 def posts(request):
-    return render(request, "post/posts.html")
+    return render(request, "post/posts.html", {"title": "Posts"})  # Empty list for now
 
 
 def post_detail(request, slug):
-    return render(request, "post/post_detail.html", {"slug": slug})
+    return render(request, "post/post_detail.html", {"slug": slug, "title": title(slug.replace('-', ' '))})
